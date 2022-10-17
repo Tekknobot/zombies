28a22fdb3327d876d742eaf67528ee28c28d49e2
Tekknobot <shanna.ragavan@gmail.com>
2022-10-16 21:36:11 -0400

    Add new blood system.

---
+++ Assets/Scripts/SpeedModifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedModifier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpeedModifierRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpeedModifierRoutine() {
+        yield return new WaitForSeconds(0.8f);
+        var velocityOverLifetime = this.GetComponent<ParticleSystem>().velocityOverLifetime;
+        velocityOverLifetime.speedModifier = 0;
+    }
+}
